41b44c28e024e7bc3badda5ed4945fb5e86dc3db
akkeykashyap <102167465+akkeykashyap@users.noreply.github.com>
2022-03-25 14:49:17 +0530

    Add files via upload

---
+++ src/component/login.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import contract from "../photos/lock.jpg";
+
+const login = ( )=>{
+    return (
+        <>
+           <div className="container login">
+              <from method="">
+                 <div className="row">
+                    <div className="col-md-2">
+                       <img sre={'lock'} alt="login" />
+                       <div className='d-flex'>
+                         <div className='main-text'>
+                          <p className="main-text-item fs-1 fw-bold ">you can enter private key</p>
+                         </div>
+                         </div>
+                       </div>
+                    </div>  
+            </from>   
+            </div>
+        </>    
+    )
+}
+export default  login;
